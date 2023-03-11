04e86373f42216f63ffba860dab4e8025bc248f8
Ketul Patel <pateketu@gmail.com>
2023-03-11 21:38:11 +0000

    Basic e2e test to cover navigation to new and edit page

--- cypress/e2e/cats.cy.ts
+++ cypress/e2e/cats.cy.ts
@@ -9,6 +9,29 @@ describe('Cats', () => {
       cy.url().should('include', '/cats')
       cy.get('h1').contains('View your cats')
     })
+
+    it('should naviage to new cat page from cats page', () => {
+      cy.visit('http://localhost:3000/cats')
+      
+      cy.get('h1').contains('View your cats');
+
+      cy.get('a.btn-primary').first().click();
+      cy.url().should('include', '/new');
+      cy.get('h1').contains('Add a new cat')
+    });
+
+    it('should naviage to edit cat page from view cat page', () => {
+      cy.visit('http://localhost:3000/cats')
+      
+      cy.get('h1').contains('View your cats');
+
+      cy.get('a.btn-primary').eq(1).click();
+      cy.get('h1').contains('Your cat');
+
+      cy.get('a.btn-primary').first().click();
+      cy.url().should('include', '/edit');
+      cy.get('h1').contains('Edit');
+    });
   })
 
   export {}
