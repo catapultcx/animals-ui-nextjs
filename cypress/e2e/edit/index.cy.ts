d9afda80e35d49cd66ca532ae8784011558331c5
DimYLiK <ddimylik@gmail.com>
2023-03-21 04:43:00 +0000

    General: - cypress test-case to Create, Update, Filter, Delete Cat;

--- cypress/e2e/edit/index.cy.ts
+++ cypress/e2e/edit/index.cy.ts
@@ -1,35 +1,124 @@
 /* eslint-disable */
 
-describe('Edit Cat', () => {
+import { Cat } from '../../../src/domain/cat';
 
-  it('should navigate to cats from main menu, Edit Smelly cat data, and edit back to original', async () => {
-    cy.visit('http://localhost:3000/')
+describe('Create, Update, Filter, Delete Cat', () => {
 
+  it('should navigate to cats from main menu, Register a new Cat, Filter and Delete', async () => {
+    const cat: Cat = {
+      id: '',
+      name: Math.random().toString(36).substring(2),
+      description: Math.random().toString(36).substring(2),
+      group: ''
+    }
+
+    goToHome();
+
+    goToManageCats();
+
+    registerNewCat(cat);
+
+    manageCats_checkCatExist(cat);
+    manageCats_filterByNameAndCheckCatExist(cat);
+    manageCats_filterByDescriptionAndCheckCatExist(cat);
+
+    manageCats_clickOnCatAndCheckBasicData(cat);
+
+    viewCat_fetchCatID();
+    viewCat_editCatDetailsButClickCancel(cat);
+    viewCat_editCatDetailsAndSave(cat);
+
+    manageCats_checkCatBeenUpdated(cat);
+    manageCat_selectCatToViewAndDelete(cat);
+
+    manageCats_checkCatDoesNotExist(cat);
+
+  })
+
+  function goToHome () {
     cy.log('--- Home ---')
 
+    cy.visit('http://localhost:3000/')
+  }
+
+  function goToManageCats () {
     cy.get('a.nav-link[href*="cats"]').click()
 
     cy.log('--- Manage Cats ---')
 
     cy.url().should('include', '/cats')
     cy.get('h1').contains('View your cats')
+  }
 
-    cy.get('td').contains('Smelly')
-    cy.get('td').contains('Cat with friends')
+  function registerNewCat (cat: { name: string; description: string }) {
+    cy.get('a')
+      .contains('Register a new Cat')
+      .click()
 
-    cy.contains('Cat with friends')
+    cy.log('--- Register a new Cat ---')
+
+    cy.get('input[name="name"]').focus().clear().type(cat.name)
+    cy.get('input[name="description"]').focus().clear().type(cat.description)
+
+    cy.get('button')
+      .contains('Save')
+      .click()
+  }
+
+  function manageCats_checkCatExist (cat: Cat) {
+    cy.log('--- Manage Cats ---')
+
+    cy.url().should('include', '/cats')
+    cy.get('h1').contains('View your cats')
+
+    cy.get('td').contains(cat.name)
+    cy.get('td').contains(cat.description)
+  }
+
+  function manageCats_checkCatDoesNotExist (cat: Cat) {
+    cy.log('--- Manage Cats ---')
+
+    cy.url().should('include', '/cats')
+    cy.get('h1').contains('View your cats')
+
+    !cy.get('td').contains(cat.name)
+    !cy.get('td').contains(cat.description)
+  }
+
+  function manageCats_filterByNameAndCheckCatExist (cat: Cat) {
+    cy.get('input[name="query"]').focus().clear().type(cat.name)
+    cy.get('button')
+      .contains('Search')
+      .click()
+    cy.get('td').contains(cat.name)
+    cy.get('td').contains(cat.description)
+  }
+
+  function manageCats_filterByDescriptionAndCheckCatExist (cat: Cat) {
+    cy.get('input[name="query"]').focus().clear().type(cat.description)
+    cy.get('button')
+      .contains('Search')
+      .click()
+    cy.get('td').contains(cat.name)
+    cy.get('td').contains(cat.description)
+  }
+
+  function manageCats_clickOnCatAndCheckBasicData (cat: Cat) {
+    cy.contains(cat.description)
       .parents('tr')
       .find('a')
       .click()
 
     cy.log('--- View Cat ---')
 
-    cy.get('h1').contains('Your cat Smelly')
+    cy.get('h1').contains('Your cat ' + cat.name)
 
-    cy.get('td').contains('Smelly')
-    cy.get('td').contains('Cat with friends')
+    cy.get('td').contains(cat.name)
+    cy.get('td').contains(cat.description)
     cy.get('td').contains('MAMMALS')
+  }
 
+  function viewCat_fetchCatID () {
     cy.get('td').contains('ID')
       .parents('tr')
       .find('td:last-child')
@@ -45,7 +134,9 @@ describe('Edit Cat', () => {
       .then(ID => {
         cy.url().should('include', '/cats/' + ID)
       })
+  }
 
+  function viewCat_editCatDetailsButClickCancel (cat: Cat) {
     cy.get('a')
       .contains('Edit Cat details')
       .click()
@@ -53,8 +144,8 @@ describe('Edit Cat', () => {
     cy.log('--- Edit Cat ---')
 
     cy.get('h1').contains('Edit Cat details')
-    cy.get('input[name="name"]').should('have.value', 'Smelly')
-    cy.get('input[name="description"]').should('have.value', 'Cat with friends')
+    cy.get('input[name="name"]').should('have.value', cat.name)
+    cy.get('input[name="description"]').should('have.value', cat.description)
 
     cy.get('@ID')
       .then(ID => {
@@ -64,7 +155,9 @@ describe('Edit Cat', () => {
     cy.get('button')
       .contains('Cancel')
       .click()
+  }
 
+  function viewCat_editCatDetailsAndSave (cat: Cat) {
     cy.log('--- View Cat ---')
 
     cy.get('@ID')
@@ -79,8 +172,8 @@ describe('Edit Cat', () => {
     cy.log('--- Edit Cat ---')
 
     cy.get('h1').contains('Edit Cat details')
-    cy.get('input[name="name"]').should('have.value', 'Smelly')
-    cy.get('input[name="description"]').should('have.value', 'Cat with friends')
+    cy.get('input[name="name"]').should('have.value', cat.name)
+    cy.get('input[name="description"]').should('have.value', cat.description)
 
     cy.get('input[name="name"]').type(' - Name updated')
     cy.get('input[name="description"]').type(' - Description updated')
@@ -88,54 +181,38 @@ describe('Edit Cat', () => {
     cy.get('button')
       .contains('Save')
       .click()
+  }
 
-
+  function manageCats_checkCatBeenUpdated (cat: Cat) {
     cy.log('--- Manage Cats ---')
 
     cy.url().should('include', '/cats')
     cy.get('h1').contains('View your cats')
 
-    cy.get('td').contains('Smelly - Name updated')
-    cy.get('td').contains('Cat with friends - Description updated')
+    cy.get('td').contains(cat.name + ' - Name updated')
+    cy.get('td').contains(cat.description + ' - Description updated')
+  }
 
-    cy.contains('Cat with friends - Description updated')
+  function manageCat_selectCatToViewAndDelete (cat: Cat) {
+    cy.contains(cat.description + ' - Description updated')
       .parents('tr')
       .find('a')
       .click()
 
     cy.log('--- View Cat ---')
 
-    cy.get('h1').contains('Your cat Smelly')
+    cy.get('h1').contains('Your cat ' + cat.name)
 
-    cy.get('td').contains('Smelly - Name updated')
-    cy.get('td').contains('Cat with friends - Description updated')
-
-    cy.get('a')
-      .contains('Edit Cat details')
-      .click()
-
-    cy.log('--- Edit Cat ---')
-
-    cy.get('h1').contains('Edit Cat details')
-    cy.get('input[name="name"]').should('have.value', 'Smelly - Name updated')
-    cy.get('input[name="description"]').should('have.value', 'Cat with friends - Description updated')
-
-    cy.get('input[name="name"]').focus().clear().type('Smelly')
-    cy.get('input[name="description"]').focus().clear().type('Cat with friends')
+    cy.get('td').contains(cat.name + ' - Name updated')
+    cy.get('td').contains(cat.description + ' - Description updated')
 
     cy.get('button')
-      .contains('Save')
+      .contains('Delete Cat')
       .click()
 
-    cy.log('--- Manage Cats ---')
+    cy.on('window:confirm', () => true);
+  }
 
-    cy.url().should('include', '/cats')
-    cy.get('h1').contains('View your cats')
-
-    cy.get('td').contains('Smelly')
-    cy.get('td').contains('Cat with friends')
-
-  })
 })
 
 export {}
